156ca45e42675225a7fbce99d12b43644d979b85
Mystic Kitty <djabranton@gmail.com>
2020-11-24 16:00:00 -0800

    Changed line one to be animals.json.

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const { animals } = require('./data/animals');
+const { animals } = require('./data/animals.json');
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
