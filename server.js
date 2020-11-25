bd430f8b06405c680e252042b2905c89a963c19e
Mystic Kitty <djabranton@gmail.com>
2020-11-24 20:59:33 -0800

    Fixed file ext for json

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const { animals } = require('./data/animals.JSON');
+const { animals } = require('./data/animals.json');
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
